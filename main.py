663ce8f57e3ea0dc76568c77da2ffb9868904231
muskanf <muskanxfatima@gmail.com>
2024-01-07 21:08:23 -0500

    minor changes

--- main.py
+++ main.py
@@ -30,8 +30,61 @@ class TCPServer:
         return data
     
 class HTTPServer(TCPServer):
+    headers = {
+        'Server': 'Basic Server',
+        'Content-Type': 'text/html',
+    }
+
+    status_codes = {
+        200: 'OK',
+        404: 'Not Found',
+    }
+
     def handle_request(self, data):
-        return b"Request received!" # send bytes, not string
+        """Handles the incoming request.
+        Compiles and returns the response
+        """
+
+        response_line = self.response_line(status_code=200)
+
+        response_headers = self.response_headers()
+
+        blank_line = b"\r\n"
+
+        response_body = b"""
+            <html>
+                <body>
+                    <h1>Request received!</h1>
+                </body>
+            </html>
+        """
+
+        return b"".join([response_line, response_headers, blank_line, response_body])
+
+    def response_line(self, status_code):
+        """Returns response line"""
+        reason = self.status_codes[status_code]
+        line = "HTTP/1.1 %s %s\r\n" % (status_code, reason)
+
+        return line.encode() # call encode to convert str to bytes
+
+    def response_headers(self, extra_headers=None):
+        """Returns headers
+        The `extra_headers` can be a dict for sending 
+        extra headers for the current response
+        """
+        headers_copy = self.headers.copy() # make a local copy of headers
+
+        if extra_headers:
+            headers_copy.update(extra_headers)
+
+        headers = ""
+
+        for h in headers_copy:
+            headers += "%s: %s\r\n" % (h, headers_copy[h])
+
+        return headers.encode() # call encode to convert str to bytes
+        
 
 if __name__ == '__main__':
     server = HTTPServer()
